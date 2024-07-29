ca362488619b9f34dc34e5f3c8fdfe2b53aa25cc
Ellenylce <ellenylcekarla27@gmail.com>
2024-07-29 15:14:49 -0300

    finalizei os comentários em cada página

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,15 @@
-const accordions = document.querySelectorAll('.accordion');
-accordions.forEach(accordion =>{
-    accordion.addEventListener('click', ()=>{
-        const body = accordion.querySelector('.accordion-body');
-        body.classList.toggle('active');
-    })
 
-})
+// Seleciona todos os elementos com a classe 'accordion'
+const accordions = document.querySelectorAll('.accordion');
+
+// Itera sobre cada elemento 'accordion'
+for (let i = 0; i < accordions.length; i++) {
+    // Adiciona um evento de clique ao elemento atual
+    accordions[i].addEventListener('click', function() {
+        // Encontra o elemento com a classe 'accordion-body' dentro do accordion atual
+        const body = accordions[i].querySelector('.accordion-body'); //encontra o elemento accordion-body dentro do accordion atual e põe na const "body"
+        // Alterna a classe 'active' no elemento encontrado
+        body.classList.toggle('active'); //Se a classe active não está presente: toggle() adiciona a classe ao elemento.
+        //Se a classe já está presente: toggle() remove a classe do elemento. metodo - "toggle"(alterar)
+    });
+}
